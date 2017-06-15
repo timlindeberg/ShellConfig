b742b21eae79fc1797213aba1e3774f6acc39513
Tim Lindeberg <tim.lindeberg@schibsted.se>
2017-06-15 10:45:53 +0200

    More woork

--- .zshrc
+++ .zshrc
@@ -1,26 +1,15 @@
-# If you come from bash you might have to change your $PATH.
-# export PATH=$HOME/bin:/usr/local/bin:$PATH
+# Shell Config v0.0.1
 
 # Path to your oh-my-zsh installation.
 export ZSH=~/.oh-my-zsh
 
-# Set name of the theme to load. Optionally, if you set this to "random"
-# it'll load a random theme each time that oh-my-zsh is loaded.
-# See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
 ZSH_THEME="agnoster-modified"
 
-export EDITOR='subl -w'
+# Use sublime as editor if it exists
+if (command -v subl >/dev/null 2>&1;) then
+  export EDITOR='subl -w'
+fi
 
-# Uncomment the following line if you want to disable marking untracked files
-# under VCS as dirty. This makes repository status check for large repositories
-# much, much faster.
-#DISABLE_UNTRACKED_FILES_DIRTY="true"
-
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
 plugins=(git svn npm knife meteor vagrant mvn z sdkman zsh-syntax-highlighting zsh-autosuggestions)
 
 source ~/.profile
@@ -28,7 +17,7 @@ source $ZSH/oh-my-zsh.sh
 
 # Aliases
 alias ccat="pygmentize -g"
-alias zshrc="subl ~/.zshrc"
+alias zshrc="$EDITOR ~/.zshrc"
 
 # Keybindings for cmd + delete and redo
 bindkey "^U" backward-kill-line
--- setup_shell.sh
+++ setup_shell.sh
@@ -1,9 +1,90 @@
-if !(command -v zsh >/dev/null 2>&1;) then
-	echo "Installing zsh"
-	#echo "$PASSWORD" | sudo -S yum install zsh
-fi
+VERSION=0.0.1
+VERBOSE=true
 
-#if [ ! -d "$HOME/.oh-my-zsh"] then
-#	echo "Installing oh-my-zsh"
-#	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"	
-#fi
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+GIT_URL="https://github.com/timlindeberg/ShellConfig"
+REPO_LOCATION="/tmp/ShellConfig-master"
+
+RED='e\033[0;31m'
+GREEN='\033[0;32m'
+MAGENTA='\033[0;35m'
+CLEAR='\033[0m'
+
+#set -x
+
+function loginfo() {
+	[ "$VERBOSE" = true ] && echo "$MAGENTA$1$CLEAR"
+}
+
+function logsuccess() {
+	[ "$VERBOSE" = true ] && echo "$GREEN$1$CLEAR"
+}
+
+function logerror() {
+	[ "$VERBOSE" = true ] && echo "$RED$1$CLEAR"
+	exit 1
+}
+
+function install_oh_my_zsh() {
+	if [ ! -d "$HOME/.oh-my-zsh" ];
+	then
+		loginfo "Installing oh-my-zsh"
+		curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+		if [ $? -ne 0 ]; then
+			logerror "Failed to install oh-my-zsh."
+		fi
+		logsuccess "Installed oh-my-zsh"
+	else
+		loginfo "oh-my-zsh is already installed"
+	fi	
+}
+
+function deploy_zhsrc() {
+	if !(grep "Shell Config v$VERSION" "$HOME/.zshrc";) then
+		loginfo "Updating .zshrc"
+		download_config_repo
+		cp "$REPO_LOCATION/.zshrc" "$HOME/.zshrc"
+	else
+		loginfo ".zshrc is up to date"
+	fi 	
+}
+
+function deploy_agnoster_theme() {
+	if !(grep "Shell Config v$VERSION" "$HOME/.zshrc";) then
+		loginfo "Updating .zshrc"
+		download_config_repo
+	else
+		loginfo ".zshrc is up to date"
+	fi 
+}
+
+function download_config_repo() {
+	if [ -d "$REPO_LOCATION" ] 
+	then
+		return 1
+	fi
+	loginfo "Downloading repo" 
+	cd /tmp
+	curl -LOk --progress-bar "$GIT_URL/archive/master.zip"
+	tar -xzf master.zip
+}
+
+function install_program() {
+	PROGRAM=$1
+	if !(command -v $PROGRAM >/dev/null 2>&1;) then
+		loginfo "Installing $PROGRAM"
+		echo "$PASSWORD" | sudo -S yum -y install $PROGRAM > /dev/null 2>&1
+		if [ $? -ne 0 ]; then
+			logerror "Failed to install $PROGRAM."
+		fi
+		logsuccess "Installed $PROGRAM"
+	else
+		loginfo "$PROGRAM is already installed"
+	fi	
+}
+
+#download_config_repo
+install_program git
+install_program zsh
+#install_oh_my_zsh
+deploy_zhsrc
